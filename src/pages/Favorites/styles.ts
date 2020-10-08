04c56a34326ce61ae57125d1599fd221c87b3753
Jean Machado <jeannzk021@gmail.com>
2020-10-07 23:01:51 -0300

    nlw commit

---
+++ src/pages/Favorites/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#060707',
+    },
+
+    teacherList: {
+        marginTop: -40,      
+    },
+})
+
+export default styles;
---
+++ src/pages/TeacherList/styles.ts
@@ -0,0 +1,59 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#060707',
+    },
+
+    teacherList: {
+        marginTop: -40,
+    },
+
+    searchForm: {
+        marginBottom: 24,
+    },
+
+    label: {
+        color: '#FFF',
+        fontFamily: 'Poppins_400Regular'
+    },
+
+    inputGroup: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+
+    inputBlock: {
+        width: '48%',
+    },
+
+    input: {
+        height: 54,
+        backgroundColor: '#FFF',
+        borderRadius: 8,
+        justifyContent: 'center',
+        paddingHorizontal: 16,
+        marginTop: 4,
+        marginBottom: 16,
+    },
+
+    submitButton: {
+        backgroundColor: '#04d361',
+        height: 56,
+        borderRadius: 8,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    submitButtonText: {
+        fontFamily: 'Archivo_700Bold',
+        color: '#FFF',
+        fontSize: 16,
+    },
+
+
+})
+
+export default styles;
